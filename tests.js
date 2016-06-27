f023c188a9a77d76e603c159ba4c961b04308a8d
Matthew Wright <matthew@smallhelm.com>
2016-06-27 12:14:06 -0600

    generatively testing for no collisions

--- tests.js
+++ tests.js
@@ -1,6 +1,16 @@
 var toId = require('./');
 var test = require('tape');
 
+var genInt = require('gent/generator/integer');
+var genString = require('gent/generator/string');
+
+var nextStr = (function(){
+  var g = genString(genInt(0, 100));
+  return function(){
+    return g.next().value;
+  };
+}());
+
 test('it', function(t){
 
   t.equals(toId('foo'), 'foo');
@@ -22,6 +32,28 @@ test('it', function(t){
   t.equals(toId('$32$'), '$36$32$36$');
 
   t.equals(toId('not-js'), 'not$45$js');
+
+  t.end();
+});
+
+test('no collisions', function(t){
+  var out_in = {};
+
+  var testStr = function(str){
+    var out = toId(str);
+    if(!out_in.hasOwnProperty(out)){
+      out_in[out] = str;
+      t.ok(true);
+      return;
+    }
+    t.equal(out_in[out], str, 'if there is a same output, it should have the same input');
+  };
+
+  var i = 0;
+  while(i < 100000){
+    testStr(nextStr());
+    i++;
+  }
 
   t.end();
 });
